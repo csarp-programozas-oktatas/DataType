51e4368c32f080c46de50fc973c29903fb49d1d3
Gyuris Csaba <gycsaba@vasvari.hu>
2018-11-29 22:50:43 +0100

    StringMaxMaganhangzoSzavakban kód kész

--- StringMaxMaganhangzoSzavakban/StringMaxMaganhangzoSzavakban/Program.cs
+++ StringMaxMaganhangzoSzavakban/StringMaxMaganhangzoSzavakban/Program.cs
@@ -8,8 +8,48 @@ namespace StringMaxMaganhangzoSzavakban
 {
     class Program
     {
+        public static int megszamolMaganhangzokSzamat(String s)
+        {
+            int db = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (char.ToUpper(s.ElementAt(i)))
+                {
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                        db++;
+                        break;
+                }
+            }
+            return db;
+        }
         static void Main(string[] args)
         {
+            String[] szavak = {
+                "almafa",
+                "diofa",
+                "szederbokor",
+                "eperfa",
+                "akacfa",
+                "csipkebokor",
+                "szilvafa",
+                "szelidgesztenyefa",
+                "bukkfa",
+                "malnabokor"
+            };
+
+            int max = -1;
+            foreach (String s in szavak)
+            {
+                int m;
+                if ((m = megszamolMaganhangzokSzamat(s)) > max)
+                    max = m;
+            }
+            Console.WriteLine("A legtöbb magánhangzót tartalmazó szóban a magánhangzók száma: " + max);
+            Console.ReadKey();
         }
     }
 }
