f4f37d28a5c57b603bd5a39f4be375ef85fce27b
Gyuris Csaba <gycsaba@vasvari.hu>
2018-11-29 23:27:30 +0100

    Összekeverés működik egy szón belül.

---
+++ stringSzavakBetuinekOsszekeverese/stringSzavakBetuinekOsszekeverese/OsszekeverhetoString.cs
@@ -0,0 +1,48 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringSzavakBetuinekOsszekeverese
+{
+    class OsszekeverhetoString
+    {
+        private string s;
+        public OsszekeverhetoString(string s)
+        {
+            this.s=s;
+        }
+        public String getOsszekevert()
+        {
+            if (s.Length < 3)
+                return s;
+            String tmp = s;
+            if (s.Length == 4)
+            {
+                int k1 = 1;
+                int k2 = 2;
+                char c1 = tmp.ElementAt(k1);
+                char c2 = tmp.ElementAt(k2);
+                tmp = tmp.Substring(0, k1) + c2 + tmp.Substring(k1 + 1);
+                tmp = tmp.Substring(0, k2) + c1 + tmp.Substring(k2 + 1);
+            }
+            else
+            {
+                Random rnd = new Random();
+                int hanyszor = (int)(rnd.Next(s.Length));
+                for (int i = 0; i < hanyszor; i++)
+                {
+                    int k1 = rnd.Next(s.Length - 2) + 1;
+                    int k2 = rnd.Next(s.Length - 2) + 1;
+                    char c1 = tmp.ElementAt(k1);
+                    char c2 = tmp.ElementAt(k2);
+                    tmp = tmp.Substring(0, k1) + c2 + tmp.Substring(k1 + 1);
+                    tmp = tmp.Substring(0, k2) + c1 + tmp.Substring(k2 + 1);
+                    //Console.WriteLine(k1+" "+k2+" "+tmp);
+                }
+            }
+            return tmp;
+        }
+    }
+}
--- stringSzavakBetuinekOsszekeverese/stringSzavakBetuinekOsszekeverese/Program.cs
+++ stringSzavakBetuinekOsszekeverese/stringSzavakBetuinekOsszekeverese/Program.cs
@@ -10,6 +10,22 @@ namespace stringSzavakBetuinekOsszekeverese
     {
         static void Main(string[] args)
         {
+            OsszekeverhetoString s1 = new OsszekeverhetoString("ab");
+            Console.WriteLine(s1.getOsszekevert());
+
+            OsszekeverhetoString s2 = new OsszekeverhetoString("abc");
+            Console.WriteLine(s2.getOsszekevert());
+
+            OsszekeverhetoString s3 = new OsszekeverhetoString("abcd");
+            Console.WriteLine(s3.getOsszekevert());
+
+            OsszekeverhetoString s4= new OsszekeverhetoString("Szoftverfejlesztő");
+            Console.WriteLine(s4.getOsszekevert());
+
+            OsszekeverhetoString s = new OsszekeverhetoString(
+                "Készítsen programot amely a billentyűzetről egy változóba beolvas egy számot");
+            Console.WriteLine(s.getOsszekevert());
+            Console.ReadKey();
         }
     }
 }
