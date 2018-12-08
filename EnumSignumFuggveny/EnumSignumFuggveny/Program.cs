cbbf2f93ada7115953d1673f9766a8e8bcdddd8c
Gyuris Csaba <gycsaba@vasvari.hu>
2018-12-08 16:10:09 +0100

    elojel függvény

--- EnumSignumFuggveny/EnumSignumFuggveny/Program.cs
+++ EnumSignumFuggveny/EnumSignumFuggveny/Program.cs
@@ -9,6 +9,21 @@ namespace EnumSignumFuggveny
     enum Elojel { POZITIV, NEGATIV, NULLA }
     class Program
     {
+        /// <summary>
+        /// Megadja a szám előjelét
+        /// </summary>
+        /// <param name="szam">A megadott szám</param>
+        /// <returns>Ha a szám nagyobb 0 akkor POZITIV, ha a szám kisebb 0 akkor NEGATIV, egyébként NULLA</returns>
+        static Elojel elojel(int szam)
+        {
+            if (szam > 0)
+                return Elojel.POZITIV;
+            else if (szam < 0)
+                return Elojel.NEGATIV;
+            else
+                return Elojel.NULLA;
+
+        }
         static void Main(string[] args)
         {
         }
